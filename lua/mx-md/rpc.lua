445da6590d71c1b73e9c0901bc3c9acd442f9042
mingxue619 <mingxue619@gmail.com>
2024-08-09 10:06:31 +0800

    rpc

--- lua/mx-md/rpc.lua
+++ lua/mx-md/rpc.lua
@@ -7,11 +7,11 @@ function M.preview()
 	local script_path = debug.getinfo(1, "S").source:sub(2)
 	-- local script_path = vim.fn.expand("<sfile>:p")
 	local project_dir = script_path:match("(.*/).-/"):match("(.*/).-/")
-	local cmd = "ALLOW_CONSOLE=1 node " .. project_dir .. "server.js " .. servername .. " > /tmp/nlog.txt"
+	local cmd = "ALLOW_CONSOLE=1 node " .. project_dir .. "server.js " .. servername .. " > /tmp/mxmd-log.txt"
 	-- local cmd = "ALLOW_CONSOLE=1 node " .. project_dir .. "demo.mjs " .. servername .. " > /tmp/nlog.txt"
-	print("cmd: " .. cmd)
+	-- print("cmd: " .. cmd)
 	-- local job_id = vim.fn.jobstart({ "node", project_dir .. "server.mjs", ">> /tmp/nlog.txt" }, {
-	local job_id = vim.fn.jobstart(cmd, {
+	M.job_id = vim.fn.jobstart(cmd, {
 		rpc = true,
 		on_stdout = function(id, data, name)
 			print("stdout: " .. table.concat(data, "\n"))
@@ -23,19 +23,18 @@ function M.preview()
 			print("Node process exited with code " .. code)
 		end,
 	})
-	M.job_id = job_id
-	vim.rpcnotify(job_id, "send_message", "Hello from Neovim!")
+	-- vim.rpcnotify(job_id, "send_message", "Hello from Neovim!")
 	-- vim.rpcnotify(3, "send_message", "Hello from Neovim!")
 	-- rpcrequest(job_id, 'nvim_eval', '"Hello " . "world!"')
 	-- rpcrequest(job_id, "nvim_eval")
-	print("job_id=" .. job_id)
+	print("job_id=" .. M.job_id)
 	-- local response vim.rpcrequest(job_id, 'nvim_eval', '"Hello " . "world!"')
 end
 function M.notify()
 	local mxmd_node_channel_id = vim.api.nvim_get_var("mxmd_node_channel_id")
 	if mxmd_node_channel_id then
 		print("Channel ID:", mxmd_node_channel_id)
-	    vim.rpcnotify(mxmd_node_channel_id, "send_message", "Hello from Neovim!")
+		vim.rpcnotify(mxmd_node_channel_id, "send_message", "Hello from Neovim!")
 	else
 		print("Variable not found")
 	end
@@ -44,7 +43,7 @@ function M.request()
 	local mxmd_node_channel_id = vim.api.nvim_get_var("mxmd_node_channel_id")
 	if mxmd_node_channel_id then
 		print("Channel ID:", mxmd_node_channel_id)
-	    vim.rpcrequest(mxmd_node_channel_id, "send_message", "Hello from Neovim!")
+		vim.rpcrequest(mxmd_node_channel_id, "send_message", "Hello from Neovim!")
 	else
 		print("Variable not found")
 	end
