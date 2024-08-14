b0880dfe02ff909494c4f4cc55e6484c4192fa07
mingxue619 <mingxue619@gmail.com>
2024-08-14 14:33:32 +0800

    clean

--- server/page-websocket.js
+++ server/page-websocket.js
@@ -1,26 +1,10 @@
 import { WebSocketServer, WebSocket } from "ws";
-// import { Server } from "socket.io";
 
 export default class PageWebSocket {
     constructor(httpServer) {
-        // this.io = new Server(httpServer, {});
         this.wss = new WebSocketServer({ server: httpServer });
     }
     setupListeners(mdn) {
-        // this.wss.on("connection", function connection(ws) {
-        //     ws.on("error", console.error);
-        //
-        //     ws.on("message", function message(msg) {
-        //         debugger
-        //         // console.log("received: %s", msg);
-        //         const data = JSON.parse(msg);
-        //         let bufnr = data.bufnr;
-        //         // let content = await mdn.getBufferLines(bufnr);
-        //         // ws.send(`Echo: ${content}`);
-        //         ws.send(`Echo: ${msg}`);
-        //     });
-        //
-        // });
         this.wss.on("connection", async (ws) => {
             console.log("A WebSocket connection has been established.");
             ws.on("error", async (err) => {
@@ -46,29 +30,11 @@ export default class PageWebSocket {
         });
     }
 
-    // setupListeners(mdn) {
-    //     this.io.on("connection", async (client) => {
-    //         debugger
-    //         console.log("a user connected");
-    //         client.on("init", (msg) => {
-    //             debugger
-    //             console.log("ws init received: " + msg);
-    //             const data = JSON.parse(msg);
-    //             let bufnr = data.bufnr;
-    //             // let content = mdn.getBufferLines(bufnr);
-    //             client.emit("refresh-content", msg);
-    //         });
-    //         client.on("disconnect", () => {
-    //             console.log("user disconnected");
-    //         });
-    //     });
-    // }
-
-    // broadcast(message) {
-    //     this.wss.clients.forEach((client) => {
-    //         if (client.readyState === WebSocket.OPEN) {
-    //             client.send(message);
-    //         }
-    //     });
-    // }
+    broadcast(message) {
+        this.wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    }
 }
