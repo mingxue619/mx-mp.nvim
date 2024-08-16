26ed2cb8910b15cce1692e3febdcc961a5409fcc
mingxue619 <mingxue619@gmail.com>
2024-08-16 14:06:25 +0800

    hl

--- app/markdown-render.js
+++ app/markdown-render.js
@@ -1,11 +1,38 @@
 class Markdown {
+    escape(str) {
+        // escape html content
+        const d = document.createElement("div");
+        d.appendChild(document.createTextNode(str));
+        return d.innerHTML;
+    }
+
     // constructor() {}
     renderMarkdown(bufferInfo) {
         const lines = bufferInfo.lines;
         if (!lines) {
             return false;
         }
-        const md = window.markdownit();
+        const md = window.markdownit({
+            html: true,
+            xhtmlOut: true,
+            breaks: false,
+            langPrefix: "language-",
+            linkify: true,
+            typographer: true,
+            quotes: "“”‘’",
+            highlight: function (str, lang) {
+                if (lang && hljs.getLanguage(lang)) {
+                    try {
+                        return `<pre class="hljs"><code>${hljs.highlight(lang, str, true).value}</code></pre>`;
+                    } catch (__) {}
+                }
+
+                // escape html content
+                const d = document.createElement("div");
+                d.appendChild(document.createTextNode(str));
+                return `<pre class="hljs"><code>${d.innerHTML}</code></pre>`;
+            },
+        });
         const newContent = lines.join("\n");
         if (this.content === newContent) {
             return;
