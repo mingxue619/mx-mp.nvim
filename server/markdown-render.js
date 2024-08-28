928ed63d9ceeb592a5ac6ab3e3b4ae687c22079d
mingxue619 <mingxue619@gmail.com>
2024-08-28 17:37:50 +0800

    marpit

--- server/markdown-render.js
+++ server/markdown-render.js
@@ -7,10 +7,43 @@ export default class MarkdownRender {
     }
 
     renderMarkdown(bufferInfo) {
-        debugger
-        const { html, css } = this.marp.render("# Hello, marp-core!");
-        bufferInfo.html = html;
-        bufferInfo.css = css;
+        debugger;
+        const lines = bufferInfo.lines;
+        if (!lines) {
+            bufferInfo.html = "";
+            bufferInfo.css = "";
+            return bufferInfo;
+        }
+        const newContent = lines.join("\n");
+        if (this.content != newContent) {
+            // 2. Add theme CSS
+            const theme = `
+            /* @theme example */
+            
+            section {
+              background-color: #369;
+              color: #fff;
+              font-size: 30px;
+              padding: 40px;
+            }
+            
+            h1,
+            h2 {
+              text-align: center;
+              margin: 0;
+            }
+            
+            h1 {
+              color: #8cf;
+            }
+            `
+            this.marp.themeSet.default = this.marp.themeSet.add(theme)
+            const { html, css } = this.marp.render(newContent);
+            this.html = html;
+            this.css = css;
+        }
+        bufferInfo.html = this.html;
+        bufferInfo.css = this.css;
         return bufferInfo;
     }
 }
